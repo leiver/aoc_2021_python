af5597c1452587b5765cbd32867ac671640e87ed
Leiv Erik Rosseland <leiv-erik.rosseland@spk.no>
2021-12-01 08:54:24 +0100

    dag 1

--- assignments/day1.py
+++ assignments/day1.py
@@ -14,7 +14,35 @@ def day1():
 def part1():
     file = open(os.path.join(sys.path[0], "inputs/input_day1.txt"), "r")
 
+    prev_depth = 0
+    count = 0
+    for line in file:
+        if int(line) > prev_depth != 0:
+            count += 1
+        prev_depth = int(line)
+
+    print(count)
 
 
 def part2():
     file = open(os.path.join(sys.path[0], "inputs/input_day1.txt"), "r")
+
+    sliding_windows = []
+    line_count = 0
+    for line in file:
+        depth = int(line)
+        sliding_windows.append(depth)
+        for i in range(1, 3):
+            if (line_count - i) >= 0:
+                sliding_windows[line_count - i] += depth
+        line_count += 1
+
+    prev_depth = 0
+    count = 0
+    for index in range(len(sliding_windows) - 2):
+        if sliding_windows[index] > prev_depth != 0:
+            count += 1
+        prev_depth = sliding_windows[index]
+
+    print(count)
+
