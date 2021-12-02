31601d1e51bbf98d0b8dc5912d6b544caf126ef6
Leiv Erik Rosseland <leiv-erik.rosseland@spk.no>
2021-12-02 07:11:21 +0100

    dag 2

--- assignments/day2.py
+++ assignments/day2.py
@@ -1,6 +1,8 @@
 from tools import timing
 import os
 import sys
+from functools import reduce
+from operator import mul
 
 
 def day2():
@@ -12,10 +14,46 @@ def day2():
 
 
 def part1():
+    #one liner
+    print(reduce(mul, reduce(lambda posdep, com: map(lambda pair: pair[0] + (pair[1] * int(com[1])), zip(posdep, {"forward": [1, 0], "up": [0, -1], "down": [0, 1]}[com[0]])), map(lambda line: line.split(" "), open(os.path.join(sys.path[0], "inputs/input_day2.txt"), "r")), [0, 0])))
+
+    #initial solution
     file = open(os.path.join(sys.path[0], "inputs/input_day2.txt"), "r")
+
+    position = 0
+    depth = 0
+    for line in file:
+        (command, amount) = line.split(" ")
+        if command == "forward":
+            position += int(amount)
+        elif command == "up":
+            depth -= int(amount)
+        elif command == "down":
+            depth += int(amount)
+
+    print(position * depth)
 
 
 def part2():
+    #one liner
+    print(reduce(mul, reduce(lambda posdepaim, com: list(map(lambda triple: triple[0] + (triple[1] * triple[2] * int(com[1])), zip(posdepaim, [1, posdepaim[2], 1], {"forward": [1, 1, 0], "up": [0, 0, -1], "down": [0, 0, 1]}[com[0]]))), map(lambda line: line.split(" "), open(os.path.join(sys.path[0], "inputs/input_day2.txt"), "r")), [0, 0, 0])[:-1]))
+
+    #initial solution
     file = open(os.path.join(sys.path[0], "inputs/input_day2.txt"), "r")
+
+    position = 0
+    aim = 0
+    depth = 0
+    for line in file:
+        (command, amount) = line.split(" ")
+        if command == "forward":
+            position += int(amount)
+            depth += aim * int(amount)
+        elif command == "up":
+            aim -= int(amount)
+        elif command == "down":
+            aim += int(amount)
+
+    print(position * depth)
 
 
