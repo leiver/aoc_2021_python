3bdd94efa0a8c9816ff9e285123f00f9af2a593e
Leiv Erik Rosseland <leiv-erik.rosseland@spk.no>
2021-12-03 08:16:53 +0100

    dag 4 prep

---
+++ assignments/day4.py
@@ -0,0 +1,22 @@
+from timing import timing
+import os
+import sys
+from more_itertools import pairwise
+from more_itertools import sliding_window
+
+
+def both_parts():
+    part1()
+
+    timing.log("Part 1 finished!")
+
+    part2()
+
+
+def part1():
+    file = open(os.path.join(sys.path[0], "inputs/input_day4.txt"), "r")
+
+
+def part2():
+    file = open(os.path.join(sys.path[0], "inputs/input_day4.txt"), "r")
+
