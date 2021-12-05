3229c58e75f98952f91842324555cef143dbd427
Leiv Erik Rosseland <leiv-erik.rosseland@spk.no>
2021-12-05 10:29:44 +0100

    dag 5 og forsøkt refaktor dag 4

--- assignments/day4.py
+++ assignments/day4.py
@@ -1,14 +1,59 @@
 from timing import timing
 import os
 import sys
+from functools import reduce
 
 
 def both_parts():
+    #both_parts_in_one()
+
     part1()
 
     timing.log("Part 1 finished!")
 
     part2()
+
+
+def both_parts_in_one():
+    file = open(os.path.join(sys.path[0], "inputs/input_day4.txt"), "r")
+    bingo_numbers = file.readline().strip().split(",")
+    bingo_numbers = reduce(lambda acc, number: {**acc, **number}, [{bingo_numbers[i]: i} for i in range(len(bingo_numbers))], {})
+    file.readline()
+
+    current_bingo_board = []
+    current_first_bingo_on_board = None
+    winning_boards = {}
+
+    for line in file.readlines():
+        line = line.strip()
+        if line:
+            row = [{bingo_numbers[number]: number} if number in bingo_numbers else {"-1": } for number in line.split()]
+            current_first_bingo_on_board = update_first_bingo_on_board(current_first_bingo_on_board, row)
+            current_bingo_board.append(row)
+        else:
+            for i in range(len(current_bingo_board[0])):
+                current_first_bingo_on_board = update_first_bingo_on_board(current_first_bingo_on_board, [row[i] for row in current_bingo_board])
+
+            if current_first_bingo_on_board:
+                winning_boards[current_first_bingo_on_board] = current_bingo_board
+
+            current_bingo_board = []
+            current_first_bingo_on_board = None
+
+    if current_first_bingo_on_board:
+        winning_boards[current_first_bingo_on_board] = current_bingo_board
+
+    first_bingo_turn = min(winning_boards.keys())
+    first_winning_board = winning_boards[first_bingo_turn]
+    print(sum([sum([bingo_numbers[number] if number == -1 or number > first_bingo_turn else 0 for number in row]) for row in first_winning_board]))
+
+
+def update_first_bingo_on_board(current_first_bingo_on_board, row):
+    if -1 not in row:
+        bingo_turn = max(row)
+        if current_first_bingo_on_board is None or bingo_turn < current_first_bingo_on_board:
+            return bingo_turn
+    return current_first_bingo_on_board
 
 
 def part1():
--- assignments/day5.py
+++ assignments/day5.py
@@ -1,6 +1,7 @@
 from timing import timing
 import os
 import sys
+import re
 
 
 def both_parts():
@@ -12,8 +13,41 @@ def both_parts():
 
 
 def part1():
-    file = open(os.path.join(sys.path[0], "inputs/input_day1.txt"), "r")
+    file = open(os.path.join(sys.path[0], "inputs/input_day5.txt"), "r")
+
+    seafloor_map = {}
+    for line in file:
+        (x1, y1, x2, y2) = map(int, re.split(',| -> ', line.strip()))
+        if x1 == x2:
+            for i in range(min(y1, y2), max(y1, y2) + 1):
+                seafloor_map[(x1, i)] = seafloor_map[(x1, i)] + 1 if (x1, i) in seafloor_map else 1
+        elif y1 == y2:
+            for i in range(min(x1, x2), max(x1, x2) + 1):
+                seafloor_map[(i, y1)] = seafloor_map[(i, y1)] + 1 if (i, y1) in seafloor_map else 1
+
+
+    print(sum([1 if point > 1 else 0 for point in seafloor_map.values()]))
 
 
 def part2():
-    file = open(os.path.join(sys.path[0], "inputs/input_day1.txt"), "r")
+    file = open(os.path.join(sys.path[0], "inputs/input_day5.txt"), "r")
+
+    seafloor_map = {}
+    for line in file:
+        (x1, y1, x2, y2) = map(int, re.split(',| -> ', line.strip()))
+        if x1 == x2:
+            for i in range(min(y1, y2), max(y1, y2) + 1):
+                seafloor_map[(x1, i)] = seafloor_map[(x1, i)] + 1 if (x1, i) in seafloor_map else 1
+        elif y1 == y2:
+            for i in range(min(x1, x2), max(x1, x2) + 1):
+                seafloor_map[(i, y1)] = seafloor_map[(i, y1)] + 1 if (i, y1) in seafloor_map else 1
+        else:
+            step_direction_x = 1 if x1 < x2 else -1
+            steps_x = range(x1, x2 + step_direction_x, step_direction_x)
+            step_direction_y = 1 if y1 < y2 else -1
+            steps_y = range(y1, y2 + step_direction_y, step_direction_y)
+            for i in range(len(steps_x)):
+                seafloor_map[(steps_x[i], steps_y[i])] = seafloor_map[(steps_x[i], steps_y[i])] + 1 if (steps_x[i], steps_y[i]) in seafloor_map else 1
+
+
+    print(sum([1 if point > 1 else 0 for point in seafloor_map.values()]))
