97ff189932b494f0c162a1ace456ae1278ba6307
Leiv Erik Rosseland <leiv-erik.rosseland@spk.no>
2021-12-07 10:29:01 +0100

    refaktor dag 7

--- assignments/day7.py
+++ assignments/day7.py
@@ -2,58 +2,30 @@ from timing import timing
 import os
 import sys
 from statistics import median
+from statistics import mean
+from functools import reduce
 
 
 def both_parts():
-    part1()
+    file = open(os.path.join(sys.path[0], "inputs/input_day7.txt"), "r")
+
+    positions = [int(n) for n in file.readline().split(",")]
+    median_pos = int(median(positions))
+
+    print(reduce(lambda acc, pos: acc + abs(pos - median_pos), positions, 0))
 
     timing.log("Part 1 finished!")
 
-    part2()
-
-
-def part1():
-    file = open(os.path.join(sys.path[0], "inputs/input_day7.txt"), "r")
-
-    positions = list(map(int, file.readline().split(",")))
-    median_pos = int(median(positions))
-    solution = 0
-    for position in positions:
-        solution += abs(position - median_pos)
-
-    print(solution)
-
-
-def part2():
-    file = open(os.path.join(sys.path[0], "inputs/input_day7.txt"), "r")
-
-
-    initial_positions = sorted(list(map(int, file.readline().split(","))))
-    positions = range(min(initial_positions), max(initial_positions)+1)
-    median_index = int(len(positions) / 2)
-    while True:
-        current_pos_sum = find_sum_distances_increasing(initial_positions, positions[median_index])
-        if len(positions) == 1:
-            print(current_pos_sum)
-            break
-        if (median_index-1) >= 0 and find_sum_distances_increasing(initial_positions, positions[median_index-1]) <= current_pos_sum:
-            direction_left = True
-        elif (median_index+1) < len(positions) and find_sum_distances_increasing(initial_positions, positions[median_index+1]) <= current_pos_sum:
-            direction_left = False
-        else:
-            print(current_pos_sum)
-            break
-
-        if direction_left:
-            positions = positions[:median_index]
-        else:
-            positions = positions[median_index:]
-        median_index = int(len(positions) / 2)
-
+    print(find_sum_distances_increasing(positions, int(mean(positions))))
 
 
 def find_sum_distances_increasing(positions, point):
-    solution = 0
-    for position in positions:
-        solution += sum(range(abs(position - point)+1))
-    return solution
+    return sum([int(distance*(distance+1)/2) for distance in [abs(position - point) for position in positions]])
+
+
+def part1():
+    both_parts()
+
+
+def part2():
+    both_parts()
