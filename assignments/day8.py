4d2575314065c21331d26fac7605daa6a8444501
Leiv Erik Rosseland <leiv-erik.rosseland@spk.no>
2021-12-09 01:00:22 +0100

    refaktor dag 8

--- assignments/day8.py
+++ assignments/day8.py
@@ -1,7 +1,10 @@
+import string
+
 from timing import timing
 import os
 import sys
-from functools import reduce
+
+segment_mapping = {"top": 0, "top left": 1, "top right": 2, "mid": 3, "bottom left": 4, "bottom right": 5, "bottom": 6}
 
 
 def both_parts():
@@ -25,75 +28,61 @@ def part1():
     print(count)
 
 
-
 def part2():
     file = open(os.path.join(sys.path[0], "inputs/input_day8.txt"), "r")
 
-    print(sum([sum_output_for_segment(line) for line in file]))
+    print(sum([find_number_for_entry(line) for line in file]))
 
 
-def sum_output_for_segment(line):
+def find_number_for_entry(line):
+
     (patterns, output) = [entry.split(" ") for entry in line.strip().split(" | ")]
-    pattern_map = reduce(lambda acc, entry: dict(acc, **{str(len(entry)): acc[str(len(entry))] + [sorted(list(entry))] if str(len(entry)) in acc else [sorted(list(entry))]}), patterns, {})
-    number_map = {}
-    number_map[1] = pattern_map["2"][0]
-    number_map[7] = pattern_map["3"][0]
-    number_map[4] = pattern_map["4"][0]
-    number_map[8] = pattern_map["7"][0]
-    segment_map = {"top right": number_map[1], "bottom right": number_map[1]}
-    segment_map["top"] = list(filter(lambda wire: wire not in number_map[1], number_map[7]))
-    mid_top_left = list(filter(lambda wire: wire not in number_map[7], number_map[4]))
-    segment_map["mid"] = mid_top_left
-    segment_map["top left"] = mid_top_left
-    rest = list(filter(lambda wire: wire not in number_map[4], number_map[8]))
-    segment_map["bottom left"] = rest
-    segment_map["bottom"] = rest
 
-    number_map[3] = list(filter(lambda number: all(wire in number for wire in number_map[1]), pattern_map["5"]))[0]
-    segment_map["bottom"] = list(filter(lambda wire: wire in number_map[3], segment_map["bottom"]))
-    segment_map["bottom left"] = list(filter(lambda wire: wire not in number_map[3], segment_map["bottom left"]))
-    segment_map["mid"] = list(filter(lambda wire: wire in number_map[3], segment_map["mid"]))
-    segment_map["top left"] = list(filter(lambda wire: wire not in number_map[3], segment_map["top left"]))
+    wire_map = create_wire_map(patterns)
 
-    number_map[2] = list(filter(lambda number: all(wire in number for wire in segment_map["bottom left"]), pattern_map["5"]))[0]
-    segment_map["top right"] = list(filter(lambda wire: wire in number_map[2], segment_map["top right"]))
-    segment_map["bottom right"] = list(filter(lambda wire: wire not in number_map[2], segment_map["bottom right"]))
-
-    number_map[5] = list(filter(lambda number: all(wire in number for wire in segment_map["top left"]), pattern_map["5"]))[0]
-    number_map[6] = list(filter(lambda number: all(wire not in number for wire in segment_map["top right"]), pattern_map["6"]))[0]
-    number_map[9] = list(filter(lambda number: all(wire not in number for wire in segment_map["bottom left"]), pattern_map["6"]))[0]
-
-    print(number_map)
-    return map_output_to_number(number_map, output)
+    return int(make_number_from_output(output, wire_map))
 
 
-def map_output_to_number(number_map, output):
+def make_number_from_output(output, wire_map):
     result = ""
-    for pattern in output:
-        pattern = sorted(list(pattern))
-        print(pattern)
-        if len(pattern) == 2:
+    for output_pattern in [set(pattern) for pattern in output]:
+        if len(output_pattern) == 2:
             result += "1"
-        elif len(pattern) == 3:
+        elif len(output_pattern) == 3:
             result += "7"
-        elif len(pattern) == 4:
+        elif len(output_pattern) == 4:
             result += "4"
-        elif len(pattern) == 7:
+        elif len(output_pattern) == 7:
             result += "8"
-        elif len(pattern) == 5:
-            if pattern == number_map[3]:
-                result += "3"
-            elif pattern == number_map[2]:
+        else:
+            filtered_wire_map = [wires & output_pattern for wires in wire_map]
+            if len(filtered_wire_map[segment_mapping["mid"]]) == 1 and len(filtered_wire_map[segment_mapping["top right"]]) == 1:
                 result += "2"
-            elif pattern == number_map[5]:
+            elif len(filtered_wire_map[segment_mapping["mid"]]) == 1 and len(filtered_wire_map[segment_mapping["bottom"]]) == 1:
+                result += "3"
+            elif len(filtered_wire_map[segment_mapping["bottom"]]) == 1 and len(filtered_wire_map[segment_mapping["top right"]]) == 1:
                 result += "5"
-        elif len(pattern) == 6:
-            if pattern == number_map[6]:
-                result += "6"
-            elif pattern == number_map[9]:
-                result += "9"
-            else:
+            elif len(filtered_wire_map[segment_mapping["mid"]]) == 1:
                 result += "0"
+            elif len(filtered_wire_map[segment_mapping["top right"]]) == 1:
+                result += "6"
+            elif len(filtered_wire_map[segment_mapping["bottom"]]) == 1:
+                result += "9"
+    return result
 
-    print(result)
-    return int(result)
+
+def create_wire_map(patterns):
+    wires = set(string.ascii_lowercase[:7])
+
+    wire_map = [wires for _ in range(7)]
+    for pattern in [set(x) for x in patterns]:
+        mask = None
+        if len(pattern) == 2:
+            mask = [segment_mapping["top right"], segment_mapping["bottom right"]]
+        elif len(pattern) == 3:
+            mask = [segment_mapping["top right"], segment_mapping["bottom right"], segment_mapping["top"]]
+        elif len(pattern) == 4:
+            mask = [segment_mapping["top right"], segment_mapping["bottom right"], segment_mapping["mid"], segment_mapping["top left"]]
+        if mask:
+            wire_map = [wire_map[i] & pattern if i in mask else wire_map[i] - pattern for i in range(len(wire_map))]
+    return wire_map
