7b949825824413bd8b01559aff41f8d9dd0f77d1
Leiv Erik Rosseland <leiv-erik.rosseland@spk.no>
2021-12-10 08:40:20 +0100

    prep dag 11

---
+++ assignments/day11.py
@@ -0,0 +1,20 @@
+from timing import timing
+import os
+import sys
+
+
+def both_parts():
+    part1()
+
+    timing.log("Part 1 finished!")
+
+    part2()
+
+
+def part1():
+    file = open(os.path.join(sys.path[0], "inputs/input_day11.txt"), "r")
+
+
+def part2():
+    file = open(os.path.join(sys.path[0], "inputs/input_day11.txt"), "r")
+
