4fbb2d4047b0fc7ea16c28908fee912f5a21b597
Leiv Erik Rosseland <leiv-erik.rosseland@spk.no>
2021-12-10 08:39:24 +0100

    dag 10

--- assignments/day10.py
+++ assignments/day10.py
@@ -1,20 +1,49 @@
 from timing import timing
 import os
 import sys
+from collections import deque
+from statistics import median
+
+bracket_map = {"(": ")", "[": "]", "{": "}", "<": ">"}
+bracket_scores_corrupt = {")": 3, "]": 57, "}": 1197, ">": 25137}
+bracket_scores_incomplete = {")": 1, "]": 2, "}": 3, ">": 4}
 
 
 def both_parts():
-    part1()
+    file = open(os.path.join(sys.path[0], "inputs/input_day10.txt"), "r")
+    corrupt_score = 0
+    incomplete_scores = []
+    for line in file:
+        stack = deque()
+        corrupt = False
+        incomplete_score = 0
+        for bracket in line.strip():
+            if bracket in bracket_map.keys():
+                stack.append(bracket)
+            else:
+                starting_bracket = stack.pop() if len(stack) > 0 else None
+                if not starting_bracket or bracket_map[starting_bracket] != bracket:
+                    corrupt_score += bracket_scores_corrupt[bracket]
+                    corrupt = True
+                    break
+
+        if not corrupt and len(stack) > 0:
+            while len(stack) > 0:
+                missing_bracket = bracket_map[stack.pop()]
+                incomplete_score = (incomplete_score * 5) + bracket_scores_incomplete[missing_bracket]
+            incomplete_scores.append(incomplete_score)
+
+    print(corrupt_score)
 
     timing.log("Part 1 finished!")
 
-    part2()
+    print(int(median(incomplete_scores)))
 
 
 def part1():
-    file = open(os.path.join(sys.path[0], "inputs/input_day10.txt"), "r")
+    both_parts()
 
 
 def part2():
-    file = open(os.path.join(sys.path[0], "inputs/input_day10.txt"), "r")
+    both_parts()
 
