7c07c2a464f1232e7700ff8a5ad9f62bf9ab6c00
Leiv-Erik Rosseland <leiv-erik.rosseland@spk.no>
2021-12-17 22:48:30 +0100

    dag 17

--- assignments/day17.py
+++ assignments/day17.py
@@ -35,35 +35,33 @@ def part2():
     y_min, y_max = tuple(map(int, y_range.split("..")))
 
     max_steps_y = (((y_min * -1) - 1) * 2) + 1
+    max_y_velocity = (y_min * -1) - 1
     max_steps_x = int(math.sqrt(x_min * 2))
     while ((max_steps_x + 1) * (max_steps_x + 2)) / 2 <= x_max:
         max_steps_x += 1
 
     unique_velocity_combinations = set()
-    for step in range(1, max_steps_y):
-        print(f"processing step {step}")
-        if step > max_steps_x:
-            valid_x_velocities_for_step = [max_steps_x]
-        else:
-            min_valid_x_velocity_for_step = int((x_min + ((step * (step + 1)) / 2) - 1) / step)
-            max_valid_x_velocity_for_step = int((x_max + ((step * (step + 1)) / 2) - step) / step)
-            print(f"\t\tmin_valid_x_velocity_for_step: {min_valid_x_velocity_for_step}\n\t\tmax_valid_x_velocity_for_step: {max_valid_x_velocity_for_step}")
-            print()
-            valid_x_velocities_for_step = list(range(min_valid_x_velocity_for_step, max_valid_x_velocity_for_step+1))
+    for step in range(1, max_steps_y + 2):
 
-        max_valid_y_velocity_for_step = int(((y_max - ((step * (step + 1)) / 2) + 1) / step) + step)
-        min_valid_y_velocity_for_step = int(((y_min - ((step * (step + 1)) / 2)) / step) + step + 1)
-        print(f"\t\tmin_valid_y_velocity_for_step: {min_valid_y_velocity_for_step}\n\t\tmax_valid_y_velocity_for_step: {max_valid_y_velocity_for_step}")
-        valid_y_velocities_for_step = list(range(min_valid_y_velocity_for_step, max_valid_y_velocity_for_step+1))
+        valid_x_velocities_for_step = []
+        for x_velocity in range(max_steps_x, x_max + 1):
+            x_after_steps = sum(range(max(1, x_velocity - step + 1), x_velocity + 1))
+            if x_min <= x_after_steps <= x_max:
+                valid_x_velocities_for_step.append(x_velocity)
+            elif x_after_steps > x_max:
+                break
+
+        valid_y_velocities_for_step = []
+        for y_velocity in range(y_min, max_y_velocity + 1):
+            y_after_steps = sum(range(y_velocity - step + 1, y_velocity + 1))
+            if y_min <= y_after_steps <= y_max:
+
+                valid_y_velocities_for_step.append(y_velocity)
+            elif y_after_steps > y_max:
+                break
 
         unique_velocity_combinations_for_step = set(list_product(valid_x_velocities_for_step, valid_y_velocities_for_step))
 
-        for unique_velocity_combination in unique_velocity_combinations_for_step:
-            x_velocity, y_velocity = unique_velocity_combination
-            x_after_steps = sum(range(x_velocity - step, x_velocity + 1))
-            y_after_steps = sum(range(y_velocity - step + 1, y_velocity + 1))
-            print(f"\tadding unique velocity {unique_velocity_combination} that will be at ({x_after_steps}, {y_after_steps}) after {step} steps")
-
-        unique_velocity_combinations.update(set(list_product(valid_x_velocities_for_step, valid_y_velocities_for_step)))
+        unique_velocity_combinations.update(unique_velocity_combinations_for_step)
 
     print(len(unique_velocity_combinations))
