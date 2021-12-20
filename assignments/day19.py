dd9b991e9fb77b9c3e9a583c4389512e6c858d3d
Leiv Erik Rosseland <leiv-erik.rosseland@spk.no>
2021-12-20 01:39:34 +0100

    dag 19

--- assignments/day19.py
+++ assignments/day19.py
@@ -10,15 +10,7 @@ from numpy import full as np_full
 
 
 def both_parts():
-    part1()
-
-    timing.log("Part 1 finished!")
-
-    part2()
-
-
-def part1():
-    file = open(os.path.join(sys.path[0], "inputs/tests/input_day19_test.txt"), "r")
+    file = open(os.path.join(sys.path[0], "inputs/input_day19.txt"), "r")
 
     beacons_in_scanner_range = {}
     for scanner_lines in file.read().strip().split("\n\n"):
@@ -43,14 +35,13 @@ def part1():
             ))
         beacon_distances_for_scanners.update({scanner_id: distances})
 
-    parsed_scanners = {"0"}
+    parsed_scanners = {"0": (0, 0, 0)}
     scanner_queue = [("0", {0: 0, 1: 1, 2: 2}, [1, 1, 1])]
     known_beacons = set(beacons_in_scanner_range["0"])
     while scanner_queue:
         current_scanner, current_swap, current_signs = scanner_queue.pop()
-        #print(current_scanner, current_swap, current_signs)
         current_scanner_beacon_distances = beacon_distances_for_scanners[current_scanner]
-        for scanner_to_parse in beacon_distances_for_scanners.keys() - parsed_scanners:
+        for scanner_to_parse in beacon_distances_for_scanners.keys() - parsed_scanners.keys():
             distances_to_parse = beacon_distances_for_scanners[scanner_to_parse]
             matched_distances = list(filter(
                 lambda distance_combination: sorted(map(abs, distance_combination[0][1])) == sorted(map(abs, distance_combination[1][1])),
@@ -60,47 +51,26 @@ def part1():
                 current_scanner_beacons, current_scanner_beacon_distance = matched_distances[0][0]
                 parsing_scanner_beacons, parsing_scanner_beacon_distance = matched_distances[0][1]
 
-                #print(matched_distances[0], matched_distances[1])
-                #print(f"{parsing_scanner_beacons[1]} == {matched_distances[1][1][0][0]}")
+                parsing_scanner_beacon = parsing_scanner_beacons[0]
                 if parsing_scanner_beacons[1] == matched_distances[1][1][0][0] or parsing_scanner_beacons[1] == matched_distances[1][1][0][1]:
                     parsing_scanner_beacon_distance = tuple(np_array(parsing_scanner_beacon_distance) * np_array([-1, -1, -1]))
+                    parsing_scanner_beacon = parsing_scanner_beacons[1]
                 swaps = {}
                 signs = []
-                #print(current_scanner_beacon_distance)
-                #print(parsing_scanner_beacon_distance)
                 for i in range(3):
                     for j in range(3):
-                        #print(f"{current_scanner_beacon_distance[current_swap[i]] * current_signs[i]} == {parsing_scanner_beacon_distance[(i+j) % 3]}")
                         if (abs(current_scanner_beacon_distance[current_swap[i]] * current_signs[i])) == abs(parsing_scanner_beacon_distance[(i+j) % 3]):
                             swaps.update({i: (i + j) % 3})
                             break
-                    #print(f"{current_scanner_beacon_distance[current_swap[i]] * current_signs[i]} / {parsing_scanner_beacon_distance[swaps[i]]} = {int((current_scanner_beacon_distance[current_swap[i]] * current_signs[i]) / parsing_scanner_beacon_distance[swaps[i]])}")
                     signs.append(int((current_scanner_beacon_distance[current_swap[i]] * current_signs[i]) / parsing_scanner_beacon_distance[swaps[i]]))
 
-                #print(swaps, signs)
-                #for beacon in beacons_in_scanner_range[scanner_to_parse]:
-                #    print(beacon)
-                #print()
                 beacons_in_scanner_range[scanner_to_parse] = [
                     tuple([beacon[swaps[i]] * signs[i] for i, beacon_coordinate in enumerate(beacon)])
                     for beacon
                     in beacons_in_scanner_range[scanner_to_parse]
                 ]
 
-                #for beacon in beacons_in_scanner_range[scanner_to_parse]:
-                #    print(beacon)
-                #print()
-
-                #distance_from_parsing_to_current = int(math.sqrt(
-                #    math.pow(math.sqrt(sum([math.pow(beacon_coord, 2) for beacon_coord in beacons_in_scanner_range[current_scanner][current_scanner_beacons[0]]])), 2) +
-                #    math.pow(math.sqrt(sum([math.pow(beacon_coord, 2) for beacon_coord in beacons_in_scanner_range[scanner_to_parse][parsing_scanner_beacons[0]]])), 2)
-                #))
-                #print(distance_from_parsing_to_current)
-                #print(beacons_in_scanner_range[current_scanner][current_scanner_beacons[0]])
-                #print(beacons_in_scanner_range[scanner_to_parse][parsing_scanner_beacons[0]])
-
-                distance_from_parsing_to_zero = np_array(beacons_in_scanner_range[current_scanner][current_scanner_beacons[0]]) - np_array(beacons_in_scanner_range[scanner_to_parse][parsing_scanner_beacons[0]])
-                #print(f"{beacons_in_scanner_range[current_scanner][current_scanner_beacons[0]]} - {beacons_in_scanner_range[scanner_to_parse][parsing_scanner_beacons[0]]} = {distance_from_parsing_to_zero}")
+                distance_from_parsing_to_zero = np_array(beacons_in_scanner_range[current_scanner][current_scanner_beacons[0]]) - np_array(beacons_in_scanner_range[scanner_to_parse][parsing_scanner_beacon])
                 beacons_in_scanner_range[scanner_to_parse] = [
                     tuple(np_array(beacon) + distance_from_parsing_to_zero)
                     for beacon
@@ -109,46 +79,23 @@ def part1():
 
                 known_beacons.update(set(beacons_in_scanner_range[scanner_to_parse]))
                 scanner_queue.append((scanner_to_parse, swaps, signs))
-                parsed_scanners.add(scanner_to_parse)
-                #print(f"done with scanner {scanner_to_parse} connected to {current_scanner}")
+                parsed_scanners.update({scanner_to_parse: tuple(distance_from_parsing_to_zero)})
 
-                #for matched_distance in matched_distances:
-                    #print(matched_distance)
-                #beacon_in_current = beacons_in_scanner_range[current_scanner][matched_distances[0][0][0][0]]
-                #beacons_in_current = [beacons_in_scanner_range[current_scanner][beacon] for beacon in matched_distances[0][0][0]]
-                #sorted_abs_coords_in_current = sorted(map(abs, beacon_in_current))
-                #beacons_in_parsed = [beacons_in_scanner_range[scanner_to_parse][beacon] for beacon in matched_distances[0][1][0]]
-                #print(beacon_in_current)
-                #print(beacons_in_current)
-                #print(beacons_in_parsed)
-                #beacon_in_parsed = list(filter(lambda beacon: sorted_abs_coords_in_current == sorted(map(abs, beacon)), beacons_in_parsed))[0]
-                #print(beacon_in_parsed)
-
-    file = open(os.path.join(sys.path[0], "inputs/tests/input_day19_test_result.txt"), "r")
-    for beacon, result in zip(sorted(known_beacons), sorted([tuple(map(int, line.strip().split(","))) for line in file.readlines()])):
-        if beacon != result:
-            print(f"{beacon[0]},{beacon[1]},{beacon[2]}")
     print(len(known_beacons))
-    #for first_scanner_distances, second_scanner_distances in combinations(beacon_distances_for_scanners.items(), 2):
-        #first_scanner_id, first_scanner_distances = first_scanner_distances
-        #second_scanner_id, second_scanner_distances = second_scanner_distances
-        #print(first_scanner_distances)
-        #print()
-        #print(second_scanner_distances)
-        #matched_distances = list(filter(lambda distance_combination: distance_combination[0][1] == distance_combination[1][1], list_product(first_scanner_distances, second_scanner_distances)))
-        #print(matched_distances)
-        #print(len(matched_distances), int((11*12)/2))
-        #matched_beacons_for_first_scanner = {beacon for beacon_pair in [matched_distance[0][0] for matched_distance in matched_distances] for beacon in beacon_pair}
-        #for beacon in matched_beacons_for_first_scanner:
-            #print(",".join(list(map(str, beacons_in_scanner_range[first_scanner_id][beacon]))))
-        #print([beacons_in_scanner_range[first_scanner_id][beacon] for beacon in matched_beacons_for_first_scanner])
-        #print()
-        #matched_beacons_for_second_scanner = {beacon for beacon_pair in [matched_distance[1][0] for matched_distance in matched_distances] for beacon in beacon_pair}
-        #for beacon in matched_beacons_for_second_scanner:
-            #print(",".join(list(map(str, beacons_in_scanner_range[second_scanner_id][beacon]))))
-        #print([beacons_in_scanner_range[second_scanner_id][beacon] for beacon in matched_beacons_for_second_scanner])
 
+    timing.log("Part 1 finished!")
+
+    max_distance = 0
+    for first_scanner, second_scanner in combinations(parsed_scanners.values(), 2):
+        manhatten_distance = abs(first_scanner[0] - second_scanner[0]) + abs(first_scanner[1] - second_scanner[1]) + abs(first_scanner[2] - second_scanner[2])
+        max_distance = max(max_distance, manhatten_distance)
+
+    print(max_distance)
+
+
+def part1():
+    both_parts()
 
 
 def part2():
-    file = open(os.path.join(sys.path[0], "inputs/input_day19.txt"), "r")
+    both_parts()
